3ed99eab0142c29cc72d3f540e33d62a9b5fec5f
mitch cail <mitch.cail@gmail.com>
2014-01-20 19:02:03 -0500

    initial commit for internify!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Internify::Application.config.secret_key_base = '8d5ed536c66b9321d653834d9b79fafdebaec67a72b4ffd91ea34e44a6c1730d7ef8f27581ad9c93964c43b9810b547cdeebc700efe53532e57d6f3f8bd984aa'

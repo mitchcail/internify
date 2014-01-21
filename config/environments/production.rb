0f311fe3b255b9c9e662ffa63f81abd68818173a
mitch cail <mitch.cail@gmail.com>
2014-01-21 10:35:16 -0500

    initialized devise and create all the view that come along with it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Internify::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # for heroku
+  # note to set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end

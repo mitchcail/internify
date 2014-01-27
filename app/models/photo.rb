0cafaf297d44c22bc049ba476f9628b33ec96742
mitch cail <mitch.cail@gmail.com>
2014-01-27 12:57:03 -0500

    added validations for a users name and for image and description of photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "300x300>" }
+
+	validates :image, presence: true
+  	validates :description, presence: true
 end
